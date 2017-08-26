bc8f090b3ea4a5bcb6dd86ec603a4c2321aca724
Eric Salant <31260966+esalant@users.noreply.github.com>
2017-08-26 16:42:33 -0400

    Update budget.py
    removed int(xxx) added float(xxx) and rounded  to the hundredth

--- budget.py
+++ budget.py
@@ -1,38 +1,38 @@
 # printing warning
 print('This is an estimator for your monthly bills!')
-
-# SOME COOL
-# CODE BLOCK
-# GOES HERE
-
-
 # amount you bring home and how much you spend
-weekly = int(input('How much money you make a week after taxes: '))
-food = int(input('How much do you spend on groceries a week: '))
-power = int(input('How much is your average monthly power bill: '))
-water = int(input('How much is your monthly water bill: '))
-cable = int(input('How much is your TV, internet, and home phone bill: '))
-phone = int(input('how much is your Cell Phone bill: '))
-
+weekly = float(input('How much money you make a week after taxes: '))
+food = float(input('How much do you spend on groceries a week: '))
+power = float(input('How much is your average monthly power bill: '))
+water = float(input('How much is your monthly water bill: '))
+cable = float(input('How much is your TV, Internet, and Home Phone bill: '))
+phone = float(input('how much is your Cell Phone bill: '))
 # monthly bill print out
+
 print('Monthly Average Bills')
 
 print('Take home pay: ', weekly * 4)
 print('How much you pay for groceries: ', food * 4)
 print('How much you pay for power: ', power)
 print('How much you pay for water: ', water)
-print('How much you pay for TV and Internet: ', cable)
+print('How much you pay for TV, Internet, and Home Phone: ', cable)
 print('How much you pay for your cell phone: ', phone)
 
 print('Left over money for the month: ', (weekly * 4) - ((food * 4) + power + water + cable + phone))
 
 print('Total for the year: ')
+weekly = round(weekly * 52, 2)
+food = round(food * 52, 2)
+power = round(power * 52, 2)
+water = round(water * 12, 2)
+cable = round(cable * 12, 2)
+phone = round(phone * 12, 2)
 
-print('Take home pay: ', weekly * 52)
-print('How much you pay for groceries: ', food * 52)
-print('How much you pay for power: ', power * 12)
-print('How much you pay for water: ', water * 12)
-print('How much you pay for TV and Internet: ', cable * 12)
-print('How much you pay for your cell phone: ', phone * 12)
+print('Take home pay: ', weekly)
+print('How much you pay for groceries: ', food)
+print('How much you pay for power: ', power)
+print('How much you pay for water: ', water)
+print('How much you pay for TV and Internet: ', cable)
+print('How much you pay for your cell phone: ', phone)
 
-print('Left over money for the year: ', ((weekly * 52) - ((food * 52) + (power * 12) + (water * 12) + (cable * 12) + (phone * 12)))
+print('Left over money for the year: ', weekly - food - power - water - cable - phone)
